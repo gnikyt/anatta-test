ef052a27d5c75cf593e6022441593c50f52e690b
Tyler King <me@gnikyt.com>
2025-02-18 15:01:10 -0330

    Modifies boot function to return error if missing arguments

--- app.js
+++ app.js
@@ -2,28 +2,39 @@ import { parseArgs, usageDisplay } from "./cli.js";
 import Client from "./shopify/client.js";
 import ShopDomain from "./shopify/domain.js";
 
+/**
+ * Boot simply does some checks on the arguments to ensure we can continue.
+ * It will return the arguments.
+ * @throws Error if missing required arguments.
+ * @returns {Object}
+ */
 function boot() {
   const args = parseArgs();
   if (Object.keys(args).length === 0) {
-    console.log(usageDisplay());
-    process.exit(1);
+    throw new Error("missing all inputs");
   }
 
   if (!args.token) {
-    console.log(`Error: Missing API token input.\n${usageDisplay()}`);
-    process.exit(1);
+    throw new Error("missing Shopify API token input");
   }
 
   if (!args.name) {
-    console.log(`Error: Missing partial product name input.\n${usageDisplay()}`);
-    process.exit(1);
+    throw new Error("missing partial product name input");
   }
+
+  return args;
 }
 
 async function main() {
-  const args = boot();
+  let args;
+  try {
+    args = boot();
+  } catch (e) {
+    console.log(`Error: ${e.message}.\n${usageDisplay()}`);
+    process.exit(1);
+  }
 
-  const client = Client(ShopDomain("quickstart-20a6d55c"), args[token]);
+  const client = Client(ShopDomain("quickstart-20a6d55c"), args.token);
   const products = await client.productsByTitle("snow");
   console.log(products);
 }
